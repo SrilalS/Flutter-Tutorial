2e49b5ac882c2c3578596eeb20d8c453d0d3e58f
Srilal Sachintha <37685396+SrilalS@users.noreply.github.com>
2020-03-30 21:42:38 +0530

    fix

--- lib/Home.dart
+++ lib/Home.dart
@@ -9,18 +9,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final dbref = Firestore.instance;
   
   @override
   Widget build(BuildContext context) {
+    var h = MediaQuery.of(context).size.height;
+    var w = MediaQuery.of(context).size.width;
     
     return Scaffold(
-      
+      backgroundColor: Colors.blue,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              StreamBuilder(
+                stream : Firestore.instance.collection('TUT').snapshots(),
+                builder :(context, snapshot) {
+
+                  if (!snapshot.hasData) {
+                  return Container(
+                    height: 128,
+                    width: w * 0.9,
+                    child: Card(
+                      elevation: 8,
+                      color: Colors.white,
+                      child: Text('No Items'),
+                    ),
+                  );
+                }
+
+                  return Container(
+                    height: h*0.9,
+                    width: w*0.9,
+                    child: ListView.builder(
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder:( _, int index) {
+                        return Container(
+                          height: 128,
+                          width: w*0.9,
+                          child: Card(
+                            color: Colors.white,
+                            child: Column(
+                              children: <Widget>[
+                                Text('Name: ' + snapshot.data.documents[index]['N']),
+                                Text('Faction: ' + snapshot.data.documents[index]['F']),
+                              ],
+                            ),
+                          ),
+                        );
+                    }),
+                  );
+                })
+            ],
+          ),
         ),
       ),
     );
